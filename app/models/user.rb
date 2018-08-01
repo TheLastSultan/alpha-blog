917b89c9699b2341be11af4cf244453e15fce6cb
Meezo <mozeiny@gmail.com>
2018-07-31 22:20:38 -0700

    done creating user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
 
     validates :email, presence: true, length: { minimum: 0, maximum: 25}, 
     uniqueness: {case_sensitive: false},
-    format: (with: VALID_EMAIL_REGEX, message: "Must be a valid email address!")
+    format: {with: VALID_EMAIL_REGEX, message: "Must be a valid email address!"}
 
 end
