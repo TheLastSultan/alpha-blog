34dc8f4e1f6626baf0a77ed749e29fc302debbe0
Meezo <mozeiny@gmail.com>
2018-07-31 21:36:24 -0700

    validate email address

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    validates :username, presence: true, 
+    uniqueness: {case_sensitive: false},
+    length: {minimum: 3, maximum: 25}
+
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, presence: true, length: { minimum: 0, maximum: 25}, 
+    uniqueness: {case_sensitive: false},
+    format: (with: VALID_EMAIL_REGEX, message: "Must be a valid email address!")
+
+end
